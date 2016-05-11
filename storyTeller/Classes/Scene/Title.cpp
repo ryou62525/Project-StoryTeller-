d36bb63fac6206cb419588989d7741a49fa0158d
norito <noritomodayo@gmail.com>
2016-05-11 11:36:32 +0900

    update to classes folder

--- storyTeller/Classes/Scene/Title.cpp
+++ storyTeller/Classes/Scene/Title.cpp
@@ -23,17 +23,36 @@ bool Title::init()
 {
     if(!Layer::init()){ return false; }
     
-    Director* director = Director::getInstance();
-    
-    Size size = director->getWinSize();
-    
-    Sprite* bgSprite = Sprite::create("ImageFile/TITLE.png");
-    
-    //bgSprite->setScale(0.7, 0.7);
-    bgSprite->setPosition(Vec2(size.width / 2, size.height / 2));
-    
-    this->addChild(bgSprite);
+    SetImageInfo();
     
     return true;
+}
+
+//画像の情報をテキストファイルから呼び出す
+void Title::SetImageInfo()
+{
+    auto filePath = FileUtils::getInstance()->fullPathForFilename("DataFile/TitleImagePath.txt");
+    std::ifstream inFile(filePath);
+    assert(inFile);
     
+    size_t imageValue;
+    std::string _filePath;
+    Vec2 pos, size;
+    
+    inFile >> imageValue;
+    Sprite* bgSprite[imageValue];
+    
+    for(int i = 0; i < imageValue; i++)
+    {
+        //画像のパス、横の座標、縦の座標、
+        //横のサイズの比率、縦のサイズの比率の順にデータを読み込む
+        inFile >> _filePath >> pos.x >> pos.y >> size.x >> size.y;
+        
+        //変数に読み込んだデータをもとに情報をセットしていく
+        bgSprite[i] = Sprite::create(_filePath);
+        bgSprite[i]->setPosition(Vec2(pos.x, pos.y));
+        bgSprite[i]->setScale(size.x, size.y);
+        
+        this->addChild(bgSprite[i]);
+    }
 }
