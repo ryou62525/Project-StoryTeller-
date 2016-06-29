5f2bcb7c3d5be77eb832ca2dcddef45b67cc92d6
norito <noritomodayo@gmail.com>
2016-06-29 12:13:39 +0900

    update

--- storyTeller/Classes/AppDelegate.cpp
+++ storyTeller/Classes/AppDelegate.cpp
@@ -4,10 +4,18 @@
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320),
+                     smallResolutionSize = cocos2d::Size(480, 320),
+                     mediumResolutionSize = cocos2d::Size(1024, 768),
+                     largeResolutionSize = cocos2d::Size(2048, 1536);
+
+enum Scenes
+{
+    Title,
+    Menu,
+    Game,
+    Result,
+};
 
 AppDelegate::AppDelegate() {
 
---
+++ storyTeller/Classes/Scene/SceneManager.hpp
@@ -0,0 +1,14 @@
+//
+//  SceneManager.hpp
+//  storyTeller
+//
+//  Created by vantan on 2016/06/29.
+//
+//
+
+#ifndef SceneManager_hpp
+#define SceneManager_hpp
+
+#include <stdio.h>
+
+#endif /* SceneManager_hpp */
