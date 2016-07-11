95c6e270919c5cb6db610f434b887614da0be7e8
nunosan <vantan@VGA-MacBookPro-23.local>
2016-07-11 15:36:21 +0900

    test 入れたよ!

--- storyTeller/Classes/AppDelegate.cpp
+++ storyTeller/Classes/AppDelegate.cpp
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "Scene/Title.hpp"
+#include "Scene/Home.hpp"
 
 USING_NS_CC;
 
@@ -52,30 +53,12 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
+    
+    glview->setDesignResolutionSize(1920, 1080, ResolutionPolicy::SHOW_ALL);
 
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
-    Size frameSize = glview->getFrameSize();
-    // if the frame's height is larger than the height of medium size.
-    if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium size.
-    else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
-    }
-
-    register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = Title::CreateScene();
+    auto scene = Home::CreateScene();
 
     // run
     director->runWithScene(scene);
